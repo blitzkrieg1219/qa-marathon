77d4b6919ca6845b7097c4f9b95ef2c0ec6a995b
DESKTOP-91LESL7\britz <blitzkrieg1219.w@gmail.com>
2023-12-25 15:18:10 +0900

    10~12KM + 諸々

--- src/node/index.js
+++ src/node/index.js
@@ -33,15 +33,57 @@ app.get("/customers", async (req, res) => {
   }
 });
 
+app.get("/customers/:customerId", async (req, res) => {
+  try {
+    const customerId = parseInt(req.params.customerId);
+    const customerData = await pool.query(
+      "SELECT * FROM customers WHERE customer_id = $1",
+      [customerId]);
+    res.send(customerData.rows);
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+});
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.post("/add-customer", async (req, res) => {
+app.post("/customers/add", async (req, res) => {
   try {
     const { companyName, industry, contact, location } = req.body;
     const newCustomer = await pool.query(
       "INSERT INTO customers (company_name, industry, contact, location) VALUES ($1, $2, $3, $4) RETURNING *",
       [companyName, industry, contact, location]
+    );
+    res.json({ success: true, customer: newCustomer.rows[0] });
+  } catch (err) {
+    console.error(err);
+    res.json({ success: false });
+  }
+});
+
+app.post("/customers/update/:customerId", async (req, res) => {
+  try {
+    const customerId = parseInt(req.params.customerId);
+    const { companyName, industry, contact, location } = req.body;
+    const newCustomer = await pool.query(
+      "UPDATE customers SET company_name=$1, industry=$2, contact=$3, location=$4, updated_date=NOW() WHERE customer_id=$5",
+      [companyName, industry, contact, location, customerId]
+    );
+    res.json({ success: true, customer: newCustomer.rows[0] });
+  } catch (err) {
+    console.error(err);
+    res.json({ success: false });
+  }
+});
+
+app.post("/customers/delete/:customerId", async (req, res) => {
+  try {
+    const customerId = parseInt(req.params.customerId);
+    const newCustomer = await pool.query(
+      "DELETE FROM customers WHERE customer_id=$1",
+      [customerId]
     );
     res.json({ success: true, customer: newCustomer.rows[0] });
   } catch (err) {
--- src/web/config.js
+++ src/web/config.js
@@ -1,7 +1,23 @@
 const config = {
-  apiUrl: 'http://localhost:3000',
-  customer: {
-    path: 'customer'
+  api: {
+    host: 'http://localhost',
+    port: '3000',
+    path: {
+      customers: {
+        list: '/customers',
+        detail: '/customers',
+        add: '/customers/add',
+        update: '/customers/update',
+        delete: '/customers/delete'
+      }
+    },
+  },
+  html: {
+    customer: {
+      addConfirm: '/customer/add-confirm.html',
+      detail: '/customer/detail.html',
+      list: '/customer/list.html'
+    }
   }
 };
 
