81ce7c9df1c063107df3d354d04b151f315ad149
DESKTOP-91LESL7\britz <blitzkrieg1219.w@gmail.com>
2023-12-27 12:51:15 +0900

    18KM prod_config.js作成

---
+++ src/web/prod_config.js
@@ -0,0 +1,24 @@
+const config = {
+    api: {
+      host: 'http://localhost',
+      port: '3000',
+      path: {
+        customers: {
+          list: '/customers',
+          detail: '/customers',
+          add: '/customers/add',
+          update: '/customers/update',
+          delete: '/customers/delete'
+        }
+      },
+    },
+    html: {
+      customer: {
+        addConfirm: '/customer/add-confirm.html',
+        detail: '/customer/detail.html',
+        list: '/customer/list.html'
+      }
+    }
+  };
+  
+  export default config;
